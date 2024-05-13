8ca37891f703e11cac566c87a6e45968ed0c0e7e
AlexandrosKaraman <alexandrosua1990@gmail.com>
2024-05-13 22:40:18 +0300

    fix: add lint disable

--- src/entities/api/index.ts
+++ src/entities/api/index.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function getAllTickets<T = any>(): Promise<T> {
   return fetch("/tickets.json")
     .then((res) => res.json())
